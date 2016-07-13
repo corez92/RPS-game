b84575cdcc65621494ada10db42b06b1a20807ca
Christopher Pourier <corez92@gmail.com>
2016-07-13 14:09:16 +0000

    Started adding the base code for the prompts

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+play_again = 1
+
+while play_again == 1:
+    human_choice = None
+    a_dict = {1:"Rock",2:"Paper",3:"Scissors"}
+    computer_choice = random.choice([1,2,3])
+    while human_choice not in [1,2,3]:
+        human_choice = input("Choose either rock (1), paper (2) or scissors(3): ")
+    print("You draw: " + a_dict[human_choice])
+    print("Herb the Robot drew: " + a_dict[computer_choice])
+    
+    if computer_choice == human_choice:
+        print("It's a tie!")
+    print("===")    
+    play_again = input("Do you wish to play again? Type 1 for Yes and anthing else for No: ")
+    print("===")
